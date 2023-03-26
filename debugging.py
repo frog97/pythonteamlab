d5cffd837eeb106308ff92971c73306a68aafdd8
hh <frog97@gmail.com>
2023-03-26 19:43:19 +0900

    3/26

--- debugging.py
+++ debugging.py
@@ -22,54 +22,12 @@ strTest()
 print(s)
 
 print("{s}, {x}")
-'''
-연습 문제 7.1: 가변 인자의 간단한 예
-다음 함수를 정의해 보자.
 
->>> def avg(x,*more):
-        return float(x+sum(more))/(1+len(more))
 
->>> avg(10,11)
-10.5
->>> avg(3,4,5)
-4.0
->>> avg(1,2,3,4,5,6)
-3.5
->>>
-*more 매개변수(parameter)가 추가적인 인자를 어떻게 수집하는지 살펴보라.
-
-'''
-def avg(x,*more):
-        return float(x+sum(more))/(1+len(more))
-print(avg(1,4,5,6,7))
 
 
 '''
-연습 문제 7.2: 튜플과 딕셔너리를 인자로 전달
-데이터 파일을 읽어 다음과 같은 튜플을 얻었다고 하자.
 
->>> data = ('GOOG', 100, 490.1)
->>>
-이 데이터로부터 Stock 객체를 생성하고자 한다. 그런데 data를 직접 전달하면 작동하지 않는다.
-
->>> from stock import Stock
->>> s = Stock(data)
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-TypeError: __init__() takes exactly 4 arguments (2 given)
->>>
-그 대신 *data를 사용하면 간단히 해결할 수 있다. 다음과 같이 해 보라.
-
->>> s = Stock(*data)
->>> s
-Stock('GOOG', 100, 490.1)
->>>
-딕셔너리를 사용할 때는 **을 붙인다. 예:
-
->>> data = { 'name': 'GOOG', 'shares': 100, 'price': 490.1 }
->>> s = Stock(**data)
-Stock('GOOG', 100, 490.1)
->>>
 연습 문제 7.3: 인스턴스의 리스트를 생성
 report.py 프로그램에서 다음과 같은 코드를 사용해 인스턴스의 리스트를 생성했다.
 
@@ -90,9 +48,10 @@ Stock(**d)를 사용해 코드를 단순화할 수 있다. 그렇게 바꿔보�
 fileparse.parse_csv() 함수에는 파일 구분자를 바꾸거나 오류를 보고하는 옵션이 있다. 위의 read_portfolio() 함수에 그런 기능이 있으면 좋을 것이다. 다음과 같이 바꿔보자.
 
 def read_portfolio(filename, **opts):
+
     '''
-    주식 포트폴리오 파일을 읽어 딕셔너리의 리스트를 생성.
-    name, shares, price를 키로 사용.
+#    주식 포트폴리오 파일을 읽어 딕셔너리의 리스트를 생성.
+#    name, shares, price를 키로 사용.
     '''
     with open(filename) as lines:
         portdicts = fileparse.parse_csv(lines,
